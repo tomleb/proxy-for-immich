1af75221dbcca268915327d87791a7333b3f7d86
Tom Lebreux <me@tomlebreux.com>
2024-11-21 16:02:29 -0500

    Remove getting started button

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -10,9 +10,5 @@
     <div class="flex place-content-center place-items-center">
       <ImmichLogo noText class="text-center" height="200" width="200" />
     </div>
-    <h1 class="text-4xl font-bold text-immich-primary dark:text-immich-dark-primary">{$t('welcome_to_immich')}</h1>
-    <Button href={AppRoute.AUTH_REGISTER} size="lg" rounded="lg">
-      <span class="px-2 font-bold">{$t('getting_started')}</span>
-    </Button>
   </div>
 </section>
